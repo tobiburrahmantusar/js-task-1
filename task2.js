82f547fea29170aa5488d66e1eba4cd73026deca
tobiburrahmantusar <tobiburrahmantusar@gmail.com>
2024-09-09 20:30:00 +0600

    solution task2

---
+++ task2.js
@@ -0,0 +1,7 @@
+let mathematics = 75.25;
+let biology = 65;
+let chemistry = 80;
+let physics = 35.45;
+let bangla = 99.50;
+let avg = (mathematics+biology+chemistry+physics+bangla)/5;
+console.log(avg.toFixed(2));
